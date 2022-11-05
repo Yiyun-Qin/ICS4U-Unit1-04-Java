1c69f8405aa4eef83ec67e4d96f4039ef6dc9ce7
Yiyun Qin <yiyun.qin@mths.ca>
2022-11-05 22:11:06 +0000

    number guess

--- Guess.java
+++ Guess.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 /**
- * The HelloWorld program implements an application that
- * simply displays energy released question to the standard output.
+ * The guessing game program
+ * let the users guess the number until they get right.
  *
  * @author  Yiyun Qin
  * @version 1.0
@@ -12,19 +12,21 @@ class Guess {
   public static void main(String[] args) {
     int insertNumber = -1;
     int turn = 0;
+    final int minimum = 1;
+    final int maximum = 6;
     String insertString;
     Scanner myobj = new Scanner(System.in);
     final double random;
     final int randomNumber;
 
-    random = Math.floor(Math.random() * 6) + 1;
+    random = Math.floor(Math.random() * maximum) + minimum;
     randomNumber = (int) Math.round(random);
     for (int loopCounter = 0; loopCounter < loopCounter + 1; loopCounter++) {
       System.out.print("Insert your guessed number between 1 and 6: ");
       insertString = myobj.next();
       try {
         insertNumber = Integer.parseInt(insertString);
-        if (insertNumber < 1 || insertNumber > 6) {
+        if (insertNumber < minimum || insertNumber > maximum) {
           System.out.println("This number is not between 1 to 6.");
         } else {
           if (insertNumber > randomNumber) {
